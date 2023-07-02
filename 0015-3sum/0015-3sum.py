8c453a84810f2959c9f9c06c09957c483bfe30e7
sbtiffanykim <sbtiffanykim@gmail.com>
2023-07-02 23:37:05 +0900

    Time: 1381 ms (72.76%), Space: 20.4 MB (90.20%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,33 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
         nums.sort()
-        answer = list()
-
+        
         for i in range(len(nums)-2):
-            # to avoid duplicate triplets
-            if i > 0 and nums[i] == nums[i-1]: continue
-
-            # using two pointers
+            if nums[i] == nums[i-1] and i > 0: continue
+            
             left = i + 1
             right = len(nums) - 1
-
+            
             while left < right:
-                add = nums[i] + nums[left] + nums[right]
-
-                if add > 0:
-                    right -= 1
-                elif add < 0:
-                    left += 1
+                sum = nums[i] + nums[left] + nums[right]
+                
+                if sum > 0: right -= 1
+                elif sum < 0 : left += 1
                 else:
-                    # add == 0
-                    answer.append([nums[i], nums[left], nums[right]])
-
-                    while left < right and nums[left] == nums[left + 1]:
+                    result.append([nums[i], nums[left], nums[right]])
+                    
+                    while left < right and nums[left] == nums[left+1]:
                         left += 1
-                    while left < right and nums[right] == nums[right - 1]:
+                    while left < right and nums[right-1] == nums[right]:
                         right -= 1
-
+                    
                     left += 1
                     right -= 1
-
-        return answer
+        
+        return result
+                
