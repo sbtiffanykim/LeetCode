2f03f3e6178074df91e22472f9c73eee02b518bf
sbtiffanykim <sbtiffanykim@gmail.com>
2024-03-19 12:22:57 +0900

    Time: 33 ms (97.38%), Space: 18.1 MB (18.79%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,5 +3,5 @@ import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        s = re.sub('[^0-9a-z]+', '', s)
-        return True if s == s[::-1] else False
+        s = re.sub('[^0-9a-z]', '', s)  # remove all non-alphanumeric characters
+        return s == s[::-1]
