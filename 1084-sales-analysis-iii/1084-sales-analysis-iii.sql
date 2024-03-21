fddf9e121f1a9b38a4721bae62acf1c48cb5e083
sbtiffanykim <sbtiffanykim@gmail.com>
2024-03-21 15:49:40 +0900

    Time: 3134 ms (5.94%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,1 +1,2 @@
-SELECT product_id, product_name FROM Product JOIN Sales USING(product_id) GROUP BY product_id HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31'
+SELECT product_id, product_name 
+FROM Sales JOIN Product USING(product_id) GROUP BY product_id HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31'
