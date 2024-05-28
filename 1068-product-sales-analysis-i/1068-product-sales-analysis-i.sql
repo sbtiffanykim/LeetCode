463cd27573dbfc3cb57cecdc7b35d73c653e816f
sbtiffanykim <sbtiffanykim@gmail.com>
2024-05-28 18:11:27 +0900

    Time: 3507 ms (6.27%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,1 +1,1 @@
-SELECT p.product_name, s.year, s.price FROM Sales s JOIN Product p ON s.product_id = p.product_id
+SELECT product_name, year, price FROM Sales s LEFT JOIN Product p ON s.product_id = p.product_id;
