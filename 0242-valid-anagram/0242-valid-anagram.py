e127ada68b27d4fb713a2ba6f302225b30951767
sbtiffanykim <sbtiffanykim@gmail.com>
2024-10-10 22:47:13 +0900

    Time: 43 ms (83.07%), Space: 17 MB (55.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return True if sorted(s) == sorted(t) else False
+        if len(s) != len(t):
+            return False
+        chr_in_s = {chr(c): 0 for c in range(ord('a'), ord('z') + 1)}
+        chr_in_t = {chr(c): 0 for c in range(ord('a'), ord('z') + 1)}
+        
+        for c in s:
+            chr_in_s[c] += 1
+        for c in t:
+            chr_in_t[c] += 1
+        
+        for c in range(ord('a'), ord('z') + 1):
+            target = chr(c)
+            if chr_in_s[target] != chr_in_t[target]:
+                return False
+        return True
