96d7885191a10848e4352f7362892dcc3a50711d
sbtiffanykim <sbtiffanykim@gmail.com>
2024-10-10 22:35:58 +0900

    Update 0217-contains-duplicate.py
    Time:  467ms (5.29%), Space: 28.3 MB (92.43%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,4 +1,15 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums_set = set(nums)
-        return (len(nums_set) != len(nums))        
+        nums.sort()
+        prev, nxt = 0, 1  # using two pointers
+        if len(nums) == 1:
+            return False
+        
+        while True:
+            if nums[prev] == nums[nxt]:  # if two adjancent numbers are identical
+                return True
+            # increase the pointers
+            prev += 1 
+            nxt += 1
+            if nxt == len(nums):  # when nxt pointer reaches to the end of the array
+                return False
