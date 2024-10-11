62674fbcee448efdb2694f225a8aefc541dda8b1
sbtiffanykim <sbtiffanykim@gmail.com>
2024-10-11 13:01:11 +0900

    Time: 76 ms (97.36%), Space: 20.1 MB (59.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,10 @@
-from collections import defaultdict
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = defaultdict(list)
-
-        for word in strs:
-            anagrams[''.join(sorted(word))].append(word)  # sort each word -> each sorted word would be a key of anagrams
+        result = collections.defaultdict(list)
+        
+        for str in strs:
+            sorted_str = ''.join(sorted(str))
+            result[sorted_str].append(str)
+        
+        return result.values()
             
-        return anagrams.values()
