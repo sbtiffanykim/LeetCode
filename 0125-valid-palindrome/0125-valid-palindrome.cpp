62f69ebf319f5ae2728a300432999bbb0cf2afd0
sbtiffanykim <sbtiffanykim@gmail.com>
2024-10-14 16:56:23 +0900

    Time: 4 ms (79.77%), Space: 8.8 MB (92.50%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string newS = "";
-        for (char c: s) {
-            if (isalnum(c)) {
-                newS += tolower(c);
+        int l = 0, r = s.length() - 1;
+        
+        while (l < r) {
+            while (l < r && !isalnum(s[l])) {
+                l++;
             }
+            while (l < r && !isalnum(s[r])) {
+                r--;
+            }
+            if (tolower(s[l]) != tolower(s[r])) {
+                return false;
+            }
+            l++;
+            r--;
         }
-        return newS == string(newS.rbegin(), newS.rend());
+        return true;
     }
 };
