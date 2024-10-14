85f77dcfe8c67e54e1ea712f632520d706d99c08
sbtiffanykim <sbtiffanykim@gmail.com>
2024-10-14 14:53:18 +0900

    Time: 60 ms (8.62%), Space: 17.1 MB (57.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        s = re.sub('[^a-z0-9]', '', s)
-        return s == s[::-1]
+        l, r = 0, len(s) - 1
+        
+        while l < r:
+            while l < r and not self.isAlphaNum(s[l]):  # ignore any non-alphanumeric character
+                l += 1
+            while l < r and not self.isAlphaNum(s[r]):  # ignore any non-alphanumeric character
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1  # move the pointers
+        return True
+    
+    def isAlphaNum(self, char):
+        return (ord('a') <= ord(char) <= ord('z') or
+               ord('A') <= ord(char) <= ord('Z') or
+               ord('0') <= ord(char) <= ord('9'))
     
