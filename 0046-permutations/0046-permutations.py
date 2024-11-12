cbfb9fbc66bbe3648978a40041f27964bb369164
sbtiffanykim <sbtiffanykim@gmail.com>
2024-11-12 14:32:18 +0900

    Time: 3 ms (35.97%), Space: 16.7 MB (60.67%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,3 +1,18 @@
 class Solution:
+    def _backtrack(self, nums: List[int], path: List[int], picked: List[bool]) -> None:
+        if len(path) == len(nums):
+            self.result.append(path[:])
+            return
+        
+        for i in range(len(nums)):
+            if not picked[i]:
+                path.append(nums[i])
+                picked[i] = True
+                self._backtrack(nums, path, picked)
+                path.pop()  # Backtracking
+                picked[i] = False
+                
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(itertools.permutations(nums))
+        self.result = []           
+        self._backtrack(nums, [], [False] * len(nums))
+        return self.result
