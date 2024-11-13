5b43ea19ac35fab1806531638f59c3c8f1b5a49c
sbtiffanykim <sbtiffanykim@gmail.com>
2024-11-13 16:06:55 +0900

    Time: 3 ms (45.34%), Space: 14.4 MB (19.96%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -15,15 +15,14 @@ private:
             res.push_back(path);
             return;
         }
-        if (i == candidates.size() || curSum > target) return;
         
-        // include candidates[i]
-        path.push_back(candidates[i]);
-        backtrack(i+1, path, curSum + candidates[i], candidates, target);
-        path.pop_back();
+        for (int j = i; j < candidates.size(); j++) {
+            if (j > i && candidates[j] == candidates[j-1]) continue;
+            if (curSum + candidates[j] > target) break;
             
-        // skip candidates[i]
-        while (i + 1 < candidates.size() && candidates[i] == candidates[i+1]) i++;
-        backtrack(i+1, path, curSum, candidates, target);
+            path.push_back(candidates[j]);
+            backtrack(j+1, path, curSum + candidates[j], candidates, target);
+            path.pop_back();
+        }
     }
 };
