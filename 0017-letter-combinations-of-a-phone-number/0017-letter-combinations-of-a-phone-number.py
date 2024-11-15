7cfeb5bee888a9eca6561cc328395ac6d85c5fac
sbtiffanykim <sbtiffanykim@gmail.com>
2024-11-15 14:49:40 +0900

    Time: 0 ms (100.00%), Space: 16.7 MB (36.02%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,21 +1,19 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        dig_to_char = {'2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'], '4': ['g', 'h', 'i'], '5': ['j', 'k', 'l'], '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'], '8': ['t', 'u', 'v'], '9': ['w', 'x', 'y', 'z']}
+        res, path = [], []
         
-        def dfs(idx, path):
+        def dfs(i: int, path: List[str]):
+            if i == len(digits):
+                res.append(''.join(path[:]))
+                return
             
-            if len(path) == len(digits):
-                result.append(path)
-                return 
-            
-            for i in range(idx, len(digits)):
-                for j in dic[digits[i]]:
-                    dfs(i+1, path+j)
+            for j in dig_to_char[digits[i]]:
+                path.append(j)
+                dfs(i + 1, path)
+                path.pop()
         
-        if not digits:
-            return []
-        
-        dic = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-        result = []
-        dfs(0, '')
-        
-        return result
+        if len(digits) > 0:
+            dfs(0, [])
+        return res
+                
