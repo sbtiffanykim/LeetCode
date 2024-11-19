bade0b9b74e876af5988f3a436b694e97b668ba9
sbtiffanykim <sbtiffanykim@gmail.com>
2024-11-19 17:09:09 +0900

    Time: 3 ms (5.38%), Space: 17 MB (13.31%) - LeetHub

---
+++ 0355-design-twitter/0355-design-twitter.py
@@ -0,0 +1,32 @@
+class Twitter:
+
+    def __init__(self):
+        self.time = 0
+        self.tweetMap = defaultdict(list)  # {userId: [tweetIds]}
+        self.followMap = defaultdict(set)  # {followerId: {followeeIds}}
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweetMap[userId].append((self.time, tweetId))
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        feed = self.tweetMap[userId][:]  # items posted by the user themself
+        for followeeId in self.followMap[userId]:  # add itmes by the user's followees
+            feed.extend(self.tweetMap[followeeId])
+        feed.sort(key = lambda x: -x[0])
+        return [tweetId for _, tweetId in feed][:10]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId:
+            self.followMap[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.followMap[followerId].discard(followeeId)
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
