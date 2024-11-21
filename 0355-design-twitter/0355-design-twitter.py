c84aa390150001f8e229c455c7db12eadd862bd3
sbtiffanykim <sbtiffanykim@gmail.com>
2024-11-21 17:02:37 +0900

    Time: 0 ms (100.00%), Space: 16.8 MB (37.41%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -1,20 +1,35 @@
 class Twitter:
 
     def __init__(self):
-        self.time = 0
-        self.tweetMap = defaultdict(list)  # {userId: [tweetIds]}
+        self.totalTweets = 0  # Decrement the value as tweets get posted to construct the minHeap
         self.followMap = defaultdict(set)  # {followerId: {followeeIds}}
+        self.tweetMap = defaultdict(list)  # {userId: [(totalTweets, tweetId)]}
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.tweetMap[userId].append((self.time, tweetId))
-        self.time += 1
+        self.tweetMap[userId].append((self.totalTweets, tweetId))
+        self.totalTweets -= 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        feed = self.tweetMap[userId][:]  # items posted by the user themself
-        for followeeId in self.followMap[userId]:  # add itmes by the user's followees
-            feed.extend(self.tweetMap[followeeId])
-        feed.sort(key = lambda x: -x[0])
-        return [tweetId for _, tweetId in feed][:10]
+        res = []
+        minHeap = []
+        
+        self.followMap[userId].add(userId)  # Include userId in their own following list to access their own tweets
+        # Iterate over the users the current user follows, including themselves
+        for followeeId in self.followMap[userId]:
+            if followeeId in self.tweetMap:
+                idx = len(self.tweetMap[followeeId]) - 1
+                tot, tweetId = self.tweetMap[followeeId][idx]  # Get the most recent tweet
+                heapq.heappush(minHeap, [tot, followeeId, tweetId, idx - 1])
+        
+        # Collect the 10 most recent tweets from the heap
+        while minHeap and len(res) < 10:
+            _, followeeId, tweetId, idx = heapq.heappop(minHeap)
+            res.append(tweetId)
+            if idx >= 0:
+                tot, tweetId = self.tweetMap[followeeId][idx]  # Get the next most recent tweet from this user
+                heapq.heappush(minHeap, [tot, followeeId, tweetId, idx - 1])
+                
+        return res
 
     def follow(self, followerId: int, followeeId: int) -> None:
         if followerId != followeeId:
@@ -22,7 +37,7 @@ class Twitter:
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
         self.followMap[followerId].discard(followeeId)
-
+        
 
 # Your Twitter object will be instantiated and called as such:
 # obj = Twitter()
