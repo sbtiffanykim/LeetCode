e03fed7a23a418b5d163e234371ae30a9a2e2413
sbtiffanykim <sbtiffanykim@gmail.com>
2024-11-22 16:34:33 +0900

    Time: 7 ms (28.56%), Space: 16.6 MB (84.86%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,38 +3,34 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         dx = [-1, 1, 0, 0]
         dy = [0, 0, -1, 1]
-        visited = [[0] * cols for _ in range(rows)]
         q = deque()  # (x, y, time)
-        res = 0
+        time = 0
+        fresh = 0
         
         def bfs():
-            nonlocal res
+            nonlocal time, fresh
             while q:
                 x, y, t = q.popleft()
-                res = t
+                time = t
                 for k in range(4):
                     nx = x + dx[k]
                     ny = y + dy[k]
-                    # If out of bounds, already visited, or not a fresh orange
-                    if nx < 0 or nx >= rows or ny < 0 or ny >= cols or visited[nx][ny] or grid[nx][ny] != 1:
+                    # If out of bounds, or not a fresh orange
+                    if nx < 0 or nx >= rows or ny < 0 or ny >= cols or grid[nx][ny] != 1:
                         continue
                     grid[nx][ny] = 2
-                    visited[nx][ny] = 1
-                    q.append((nx, ny , t + 1))
+                    fresh -= 1
+                    q.append((nx, ny, t + 1))
                     
-        # Add rotten oranges to the queue
         for i in range(rows):
             for j in range(cols):
+                # Count fresh oranges
+                if grid[i][j] == 1:
+                    fresh += 1
+                # Add rotten oranges to the queue                
                 if grid[i][j] == 2:
                     q.append((i, j, 0))
-                    visited[i][j] = 1
-        
+                
         bfs()
         
-        # If there are still fresh oranges left, return -1
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 1:
-                    return -1
-                
-        return res
+        return -1 if fresh != 0 else time
